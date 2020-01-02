1b07a9d33217db87d89a6203707fbd7b8a8bc2ae
面川泰明 <omokawa@senk-inc.co.jp>
2020-01-02 18:00:36 +0900

    setup app blueprint

---
+++ src/store/actions.js
@@ -0,0 +1,36 @@
++/* eslint-disable no-unused-vars */
+import * as types from './mutation-types'
+import {Auth, List, Task} from '../api'
+/* eslint-enable no-unused-vars */
+
+export default {
+  login: ({commit}) => {
+    // TODO:
+    throw new Error('login action should be impremented')
+  },
+
+  fetchLists: ({commit}) => {
+    // TODO:
+    throw new Error('fetchLists action should be impremented')
+  },
+
+  addTask: ({commit}) => {
+    // TODO:
+    throw new Error('addTask action should be impremented')
+  },
+
+  updateTask: ({commit}) => {
+    // TODO:
+    throw new Error('updateTask action should be impremented')
+  },
+
+  removeTask: ({commit}) => {
+    // TODO:
+    throw new Error('removeTask action should be impremented')
+  },
+
+  logout: ({commit}) => {
+    // TODO:
+    throw new Error('logout action should be impremented')
+  }
+}
--- src/store/index.js
+++ src/store/index.js
@@ -6,7 +6,19 @@ import mutations from './mutations'
 
 Vue.use(Vuex)
 
+// ログイン状態Authと、タスクの状態BoardをVuexで一元管理する
+const state = {
+  auth: {
+    token: null,
+    userId: null
+  },
+  board: {
+    lists: [] // TaskListを空配列で初期化
+  }
+}
+
 export default new Vuex.Store({
+  state, // 定義したstateを`state`オプションに指定
   getters,
   actions,
   mutations,
